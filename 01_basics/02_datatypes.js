22af58ac5e0a15e45a4351488218b8217e50a006
PRINCE THAKUR <130538804+princethakur2006@users.noreply.github.com>
2023-07-02 11:26:05 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,34 @@
+"use strict"; //treat all js code as newer version of js
+
+// alert( 3 + 3 ) // we are using  nodejs, not browser
+
+console.log(3 +
+     3) //code readability that should be high
+     
+     console.log("hitesh")
+
+
+
+let name="hitesh" //string datatype
+let age="18"  //number datatyppe
+let isLoggedIn = false
+
+
+
+// number => 2 to the power of 53 
+
+// bigint (used for big number )
+
+// string => ""
+
+//null =>datatype and also a standalone value and its also a object
+
+//undefined => output is undefined
+
+//symbol => to find uniqueness 
+
+//object
+
+//boolean => true or false
+
+console.log(typeof null);
